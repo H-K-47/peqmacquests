224c673a4636313579611acafd52365d94daeccb
regneq <rregenn@gmail.com>
2015-03-15 13:30:33 -0700

    moved random trigger spawn from minotaur_slaver.lua to a_minotaur_slaver.lua.

--- steamfont/a_minotaur_slaver.lua
+++ steamfont/a_minotaur_slaver.lua
@@ -1,8 +1,11 @@
 --NPC: a_minotaur_slaver   Zone: steamfont
 
 function event_death_complete(e)
-	if(math.random(100) > 98) then
+	ran = math.random(100);
+	if(ran > 98) then
 		eq.unique_spawn(56152,0,0,-1294,1360,-103);
+	elseif(ran < 5) then
+		eq.unique_spawn(56161,0,0,-2179,1319,-101.2,0);
 	end
 	e.self:Say("I die soon! Meldrath, help me!");
 end
