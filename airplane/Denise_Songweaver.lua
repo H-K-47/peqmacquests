a132d5e58be8c868e2e1224117f566af2a918d03
regneq <rregenn@gmail.com>
2015-03-20 14:43:40 -0700

    fixed a few typos

--- airplane/Denise_Songweaver.lua
+++ airplane/Denise_Songweaver.lua
@@ -10,7 +10,7 @@ function event_say(e)
 	elseif(e.message:findi("voice")) then 	--bard test of voice
 		e.self:Say("The sweet sound rising forth from our throats are what makes us truly great.  Go forth and give voice to your songs, and return to me a platinum disc, a music box, and a light woolen mantle. If you do this I shall give you the Songweaver's Mantle. Good luck!");
 	elseif(e.message:findi("tone")) then 	--bard test of tone
-		e.self:Say("Tone is important to all singers. Prove to me that you can keep your tone even and pure by bringng me an ochre tessera, a songbird statuette, and a light woolen mask. If you do this, I will give you the mask of song.");
+		e.self:Say("Tone is important to all singers. Prove to me that you can keep your tone even and pure by bringing me an ochre tessera, a songbird statuette, and a light woolen mask. If you do this, I will give you the mask of song.");
 	end
 end
 
