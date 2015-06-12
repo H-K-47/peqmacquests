a01104bff7ad8330ced9b1ee0e8be9a4fb4a1e67
regneq <rregenn@gmail.com>
2015-06-11 22:38:24 -0700

    fixed Crusader's Test of Betrayal (#5) in Firiona Vie.

--- firiona/Tracker_Azeal.lua
+++ firiona/Tracker_Azeal.lua
@@ -1,21 +1,9 @@
 --Tracker Azeal is actually Hero Goxnok in disguise. He is part of the fifth quest in the Greenmist line.
 --He will check the zone time every 30 seconds, and if it's 9 PM, he will respawn as a wandering form of himself that will path off and meet whoever is doing the quest.
 
-function event_spawn(e)
-	eq.set_timer("ZoneTime",30000);
-end
-
 function event_say(e)
 	if(e.message:findi("hail")) then
 		e.self:Emote("looks over at you and gives you an awkward smile, then quickly looks away. You notice he isn't like most of the elves you've seen. You can't quite put your finger on it, but for one thing he smells like he hasn't bathed in weeks.");
-	end
-end
-
-function event_timer(e)
-	if((e.timer == "ZoneTime") and (zonehour == 21)) then
-		eq.spawn2(84028,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
-		eq.stop_timer("ZoneTime");
-		eq.depop_with_timer();
 	end
 end
 
