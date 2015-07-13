0134f34b9b83d39012d3c9180f31849e076470bf
regneq <rregenn@gmail.com>
2015-07-13 12:43:20 -0700

    Change the way black reaver / Lord Rak`Ashiir spawns in the spawn trigger event.

--- citymist/90005.lua
+++ citymist/90005.lua
@@ -3,8 +3,10 @@ function event_death_complete(e)
 	local chance_spawn = math.random(100);
 	if(chance_spawn <= 80) then
 		eq.spawn2(90005,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
-	elseif(eq.get_entity_list():IsMobSpawnedByNpcTypeID(90005) == false) then
-		eq.spawn2(90013,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
+	elseif(eq.get_entity_list():IsMobSpawnedByNpcTypeID(90005)) then
+		eq.spawn2(eq.ChooseRandom(90048,90177,90083,90179,90091,90181),0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
+	else
+		eq.unique_spawn(90013,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
 	end
 end
 
