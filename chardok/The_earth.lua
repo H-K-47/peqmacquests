6461b5142cfa8e19bc947f72148243762a6a491c
regneq <rregenn@gmail.com>
2015-08-14 00:02:47 -0700

    More emotes for Chardok.

--- chardok/The_earth.lua
+++ chardok/The_earth.lua
@@ -2,7 +2,7 @@ function event_spawn(e)
 	local xloc = e.self:GetX();
 	local yloc = e.self:GetY();
 	local zloc = e.self:GetZ();
-	eq.set_proximity(xloc - 25, xloc + 25, yloc - 25, yloc + 25, zloc - 3, zloc + 6);
+	eq.set_proximity(xloc - 45, xloc + 45, yloc - 45, yloc + 45, zloc - 3, zloc + 6);
 end
 
 function event_enter(e)
