4bc51fb7128eb0f9d8f2667f9812b59f605ce0e0
regneq <rregenn@gmail.com>
2015-08-13 22:48:34 -0700

    fixed the spawn2 trigger for one of the emote in Chardok.

--- chardok/The_earth.lua
+++ chardok/The_earth.lua
@@ -7,6 +7,6 @@ end
 
 function event_enter(e)
 	e.self:Emote("at your feet erupts!");
-	eq.spawn2(eq.ChooseRandom(103158,103158,103159),0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetY(),e.self:GetHeading());
+	eq.spawn2(eq.ChooseRandom(103158,103158,103159),0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading());
 	eq.depop_with_timer();
 end
