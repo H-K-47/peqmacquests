25a15426f52516e1c94311720e525b9e3b345a81
regneq <rregenn@gmail.com>
2015-08-29 10:59:01 -0700

    Added the Goblin Battlemaster Quest in Firiona Vie.

--- firiona/Captain_Keatar.lua
+++ firiona/Captain_Keatar.lua
@@ -7,7 +7,7 @@ function event_say(e)
 		if(e.other:GetFaction(e.self) > 4) then
 			e.self:Say("I cannot trust you full. When you have performed more deeds in the name of the Union of Vie, then and only then shall I trust you.");
 		else
-			e.self:Say("Your faction is high enough, Come back later and I might have something for you."); -- temporary until we get the text from live.
+			e.self:Say("The goblin battlemasters are formidable fighters.  They have defeated many of the patrols I sent into the plains.  Bring me proof of four of these fallen goblins and I shall reward you with a piece of Faydark ringmail armor.");
 		end
 	end
 end
@@ -23,6 +23,17 @@ function event_trade(e)
 		e.other:Faction(193,-1);
 		e.other:Faction(250,-1);
 		e.other:QuestReward(e.self,0,8,0,0,0,1000);
+	elseif(e.other:GetFaction(e.self) < 5) then
+		if(item_lib.check_turn_in(e.self, e.trade, {item1 = 12923,item2 = 12923,item3 = 12923,item4 = 12923})) then
+			e.self:Say("Good work! Please accept this piece of armor. It was crafted in Felwithe for the swiftly moving rangers of this outpost.");
+			e.other:Faction(418,5);
+			e.other:Faction(92,3);
+			e.other:Faction(314,3);
+			e.other:Faction(193,-1);
+			e.other:Faction(250,-1);
+			e.other:QuestReward(e.self,0,8,0,0,eq.ChooseRandom(12924,12925,12926,12927,12928,12929,12930,12931,12932,12933),5000);
+		end
+		item_lib.return_items(e.self, e.other, e.trade)
 	end
 	item_lib.return_items(e.self, e.other, e.trade)
 end
