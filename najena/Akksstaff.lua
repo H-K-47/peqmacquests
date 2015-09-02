d92c5e46df28f4ff58f8c6ad43803c7af3108e37
regneq <rregenn@gmail.com>
2015-09-02 13:18:28 -0700

    Update Mage Epic Scripts.

--- najena/Akksstaff.lua
+++ najena/Akksstaff.lua
@@ -13,8 +13,7 @@ function event_trade(e)
 	
 	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 28027,item2 = 28028,item3 = 28029,item4 = 28030})) then
 		e.self:Say("Very well. Take these words back to that shriveled old gnome, Rykas, if you wish to complete this quest. Now leave me, " .. e.other:GetName() .. ", it sickens me to be this cordial for so long. And my pet needs exercise. Go!");
-		e.other:Ding();
-		e.other:SummonItem(28004);
+		e.other:QuestReward(e.self,0,0,0,0,28004);
 		eq.depop_with_timer();
 	end
 	item_lib.return_items(e.self, e.other, e.trade)
