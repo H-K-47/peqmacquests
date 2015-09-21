f4ab9aabec218406e6bb9f73e0c7403f8e313ef5
regneq <rregenn@gmail.com>
2015-09-21 00:19:24 -0700

    Update a few scripts.

--- lakerathe/Eldreth.lua
+++ lakerathe/Eldreth.lua
@@ -18,13 +18,12 @@ function event_trade(e)
 	local item_lib = require("items");
 		if(item_lib.check_turn_in(e.self, e.trade, {item1 = 13087, item2 = 13087, item3 = 28012, platinum = 100})) then
 		e.self:Say("Hmm, interesting. This document is not only encoded, but written in a very obscure language. From what I can gather, it's a variant of elder Teir'Dal, but not one I've encountered. I can not fully translate this, but I know one who can. Find Yendar and give him this.");
-		e.other:SummonItem(28053);
-		e.other:Faction(240,20);--Order of Three
-		e.other:Faction(21,-10);--Bloood Sabers
-		e.other:Faction(135,10);--Guards of Qeynos
-		e.other:Faction(235,10);--Opal Dark Briar
-		e.other:AddEXP(500);
-		e.other:Ding();
+		e.other:Faction(240,400);--Order of Three
+		e.other:Faction(21,-100);--Bloood Sabers
+		e.other:Faction(135,60);--Guards of Qeynos
+		e.other:Faction(235,-60);--Opal Dark Briar
+		e.other:QuestReward(e.self,0,0,0,0,28053,500);
+		eq.depop_with_timer();
 		end
 	item_lib.return_items(e.self, e.other, e.trade)
 end
