e10e14c8026ba713c5219e021ba24e535147c9d3
regneq <rregenn@gmail.com>
2015-09-23 16:41:26 -0700

    update Shaman Cudgel #7 scripts.

--- lakeofillomen/Klok_Sargin.lua
+++ lakeofillomen/Klok_Sargin.lua
@@ -1,8 +1,8 @@
 --Shaman Skull Quest 6
 function event_say(e)
-	if(e.message:findi("I am looking for a skull of the Sisters of Scale") and e.other:GetFaction(e.self) <= 5) then
+	if(e.message:findi("Sisters of Scale") and e.other:GetFaction(e.self) <= 5) then
 		e.self:Say("Looking for the skull I had for sale? I am afraid I sold it to a staunch lizard champion. I would gladly give you his name if you would do me a [slight favor].");
-	elseif(e.message:findi("what slight favor") and e.other:GetFaction(e.self) <= 5) then
+	elseif(e.message:findi("slight favor") and e.other:GetFaction(e.self) <= 5) then
 		e.self:Say("The lake garrison has requested that I work on a helm similar to the ones worn by sarnak dragoons. The problem is.. I need one to examine. You go get me one and I will tell you who purchased the skull.");
 	end
 end
