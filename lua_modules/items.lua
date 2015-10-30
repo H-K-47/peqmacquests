88cec3eba263456615333801c46a523c3fc5b109
Ludwig <cavedude@peqtgc.com>
2015-10-30 15:07:56 -0700

    Changed SummonItem to properly handle charged items when they are handed back.

--- lua_modules/items.lua
+++ lua_modules/items.lua
@@ -172,10 +172,9 @@ function items.return_items(npc, client, trade, text)
 			-- If the npc does not have this item in their quest loot, then it needs to be returned.
 			if(not npc:GetQuestLoot(inst:GetID())) then
 				npc:DeleteQuestLoot();
-				-- local itemid = inst:GetID();
-				-- local charges = inst:GetCharges();
-				-- client:SummonItem(itemid,charges);
-				client:PushItemOnCursor(inst);
+				local itemid = inst:GetID();
+				local charges = inst:GetCharges();
+				client:SummonItem(itemid, charges, false, 9999, true);
 				if(text == true and npc:CanTalk()) then
 					npc:Say(string.format("I have no need for this item %s, you can have it back.", client:GetCleanName()));
 				end
