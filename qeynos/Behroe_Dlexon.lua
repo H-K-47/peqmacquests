ebfc7ae11f3b406dba18922423aec1ef7117e23f
H-K-47 <majinmlf-hk47@yahoo.com>
2016-02-04 20:40:39 +0100

    Quest: Aenia and Behroe
    Aenia Ghenson: Added faction checks and messages;Added death message
    
    Behroe Dlexon: Added faction checks and messages;Added death message;
    Corrected Faction Reward

--- qeynos/Behroe_Dlexon.lua
+++ qeynos/Behroe_Dlexon.lua
@@ -3,9 +3,13 @@ function event_say(e)
     e.self:QuestSay(e.other,string.format("Ah, greetings, %s!  How are you this evening?  Hopefully, you are faring much better than I..  I'm stuck on the night watch here, and never get to see my lovely [Aenia].  Ah..  she is so sweet..  I wrote her this beautiful [ballad], but I fear she may never hear it.",e.other:GetName()));
   elseif(e.message:findi("aenia")) then
     e.self:QuestSay(e.other,"Aenia lives in North Qeynos in a little blue house near the Temple of Life with her overprotective father.  Last time he caught me there, he nearly killed me!");
-  elseif(e.message:findi("ballad")) then
-    e.self:QuestSay(e.other,"I wrote this little song for my dearest Aenia, but I can't sing it to her because I'm stuck here on watch duty.  You have a nice voice, " .. e.other:GetName() .. ", maybe you could go and sing my song to her for me, huh?  Just make sure you don't sing to Aenia when her father's around, 'cause like I said, he's already tried to kill me for seeing her.");
-    e.other:SummonItem(18026);
+  elseif(e.message:findi("ballad")) then  	
+  	if(e.other:GetFaction(e.self) > 5) then -- must be at least indifferent
+  		e.self:Say("The League of Antonican Bards is very displeased with your recent actions. And I don't particularly care for you, or your stench, either!");
+  	else
+	    e.self:QuestSay(e.other,"I wrote this little song for my dearest Aenia, but I can't sing it to her because I'm stuck here on watch duty.  You have a nice voice, " .. e.other:GetName() .. ", maybe you could go and sing my song to her for me, huh?  Just make sure you don't sing to Aenia when her father's around, 'cause like I said, he's already tried to kill me for seeing her.");
+	    e.other:SummonItem(18026);
+	   end
   end
 end
 
@@ -14,15 +18,17 @@ function event_trade(e)
   if(item_lib.check_turn_in(e.self, e.trade, {item1 = 18027})) then
     e.self:Say(string.format("Ah, thank you, kind %s.  You've made two foolish lovebirds very happy.  Please, take this..  Though it is not much, it will help keep you warm on those chilly Karana nights.  It is very good to have a friend such as yourself, and I will one day repay you for your kindness and generosity.",e.other:GetName()));
     e.other:SendSound();
-    e.other:Faction(192,2,0);
-    e.other:Faction(184,2,0);
-    e.other:Faction(135,2,0);
-    e.other:Faction(273,-2,0);
-    e.other:Faction(207,-2,0);
+    -- confirmed live factions
+    e.other:Faction(192,8,0);
+    e.other:Faction(184,1,0);
+    e.other:Faction(135,1,0);
+    e.other:Faction(273,-1,0);
+    e.other:Faction(207,-1,0);
     e.other:AddEXP(250);
   elseif(item_lib.check_turn_in(e.self, e.trade, {item1 = 18021})) then
     e.self:Emote(string.format("yawns and says, 'Oh, report time already again?  Yeah, here ya go, %s.  Be careful around here at night, I've been seeing some rough looking characters lurking about.",e.other:GetName()));
     e.other:SendSound();
+     -- confirmed live factions
     e.other:SummonItem(18023);
     e.other:Faction(192,10,0);
     e.other:Faction(184,1,0);
@@ -32,4 +38,8 @@ function event_trade(e)
     e.other:AddEXP(250);
   end
   item_lib.return_items(e.self, e.other, e.trade, e.text)
+end
+
+function event_death_complete(e)
+	e.self:Say("Your actions will not go unnoticed! The League of Antonican Bards has many members all over this continent.");
 end
--- qeynos2/Aenia_Ghenson.lua
+++ qeynos2/Aenia_Ghenson.lua
@@ -4,7 +4,15 @@ function event_say(e)
 	elseif(e.message:findi("boyfriend")) then
 		e.self:Say("My boyfriend's name is Behroe Dlexon.  He is so dreamy..  <sigh>..  But, I haven't been able to see him lately.  He works nights down at the docks, and with my father here all day, we just can't seem to get together.  Oh, how I wish I could speak with him..");
 	elseif(e.message:findi("oh glistening crimson rose, you would be such a catch. oh beautiful thornless rose, your essence has no match")) then
-		e.self:Say("Oh..   My Behroe is so sweet.  Oh, how I long to see him.  Please, oh please, kind Kajigger, can you do me but one favor? Here, quickly, before my father returns.  Take this letter to my love, Behroe.  I have no money to offer you, but as you are a friend of his, so are you a friend of mine, and I will not forget your good deeds, Kajigger.  Thank you and be safe.");
-		e.other:SummonItem(18027);
+		if(e.other:GetFaction(e.self) > 5) then -- must be at least indifferent
+			e.self:Say("Heh.. With all you've done, I'm surprised you're still alive.");
+		else
+			e.self:Say("Oh..   My Behroe is so sweet.  Oh, how I long to see him.  Please, oh please, kind Kajigger, can you do me but one favor? Here, quickly, before my father returns.  Take this letter to my love, Behroe.  I have no money to offer you, but as you are a friend of his, so are you a friend of mine, and I will not forget your good deeds, Kajigger.  Thank you and be safe.");
+			e.other:SummonItem(18027);
+		end
 	end
+end
+
+function event_death_complete(e)
+	e.self:Say("You can't break the Circle.. kill one, and.. there will always be another to take his place.. and.. take revenge.");
 end
